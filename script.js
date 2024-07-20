cd03e2580183a06e37c11c95acb76e527c772370
Sahil Suthar <sahil26dd@gmail.com>
2024-07-20 12:15:19 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 26;
+console.log(document.querySelector('.guess').value);
+*/
+
+///////////////////////////////////////////////
+// Check button -- Game logic
+
+// Secret number
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
+
+// Score counter
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //   When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // Checking guess is high or low
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      // document.querySelector('.score').textContent = score;
+      displayScore(score);
+    } else {
+      displayMessage('💥 You lost the game!');
+      // document.querySelector('.score').textContent = 0;
+      displayScore(0);
+    }
+  }
+});
+
+////////////////////////////////////////////////
+// Again button -- Reset logic
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  // document.querySelector('.score').textContent = score;
+  displayScore(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
